e375f395dd358e1328936b794a4c35255224a227
sidchopp <sidchopp@gmail.com>
2024-03-05 11:43:34 -0700

    query variables

--- index.js
+++ index.js
@@ -7,8 +7,11 @@ import db from "./db.js";
 const resolvers = {
   Query: {
     games: () => db.games,
+    game: (_, args) => db.games.find((game) => game.id === args.id),
     authors: () => db.authors,
+    author: (_, args) => db.authors.find((author) => author.id === args.id),
     reviews: () => db.reviews,
+    review: (_, args) => db.reviews.find((review) => review.id === args.id),
   },
 };
 
