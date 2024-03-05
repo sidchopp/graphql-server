77b27f9c11b418c53c20c8a1972f79a15f46fd02
sidchopp <sidchopp@gmail.com>
2024-03-05 14:47:57 -0700

    related Data resolvers

--- index.js
+++ index.js
@@ -7,11 +7,28 @@ import db from "./db.js";
 const resolvers = {
   Query: {
     games: () => db.games,
+    // args - query variables
     game: (_, args) => db.games.find((game) => game.id === args.id),
     authors: () => db.authors,
     author: (_, args) => db.authors.find((author) => author.id === args.id),
     reviews: () => db.reviews,
     review: (_, args) => db.reviews.find((review) => review.id === args.id),
+  },
+  Game: {
+    reviews: (parent) =>
+      // parent- Game
+      db.reviews.filter((review) => review.game_id === parent.id),
+  },
+  Author: {
+    reviews: (parent) =>
+      // parent- Author
+      db.reviews.filter((review) => review.author_id === parent.id),
+  },
+  Review: {
+    // parent- Review
+    author: (parent) =>
+      db.authors.find((author) => author.id === parent.author_id),
+    game: (parent) => db.games.find((game) => game.id === parent.game_id),
   },
 };
 
